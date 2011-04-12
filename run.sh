42e0be877dd9f28634d996b3af74b981282298b5
Sebastien Paumier <sebastien.paumier@univ-mlv.fr>
2011-04-12 12:02:16 +0000

    Renaming log files to know which programs they test+removing unnecessary log files

--- run.sh
+++ run.sh
@@ -3,8 +3,10 @@ for i in `ls *.ulp`
 do
 	echo ================ $i ===================
 	rm -f errr.txt 
-	valgrind -q --error-exitcode=66 --leak-check=full ~/workspace/C++/bin/UnitexToolLogger RunLog $i  -s summ.txt -e errr.txt --cleanlog
-	if [ "$?" -n "0" ] ;
+	#valgrind -q --error-exitcode=66 --leak-check=full ~/workspace/C++/bin/UnitexToolLogger RunLog $i  -s summ.txt -e errr.txt --cleanlog
+	~/workspace/C++/bin/UnitexToolLogger RunLog $i  -s summ.txt -e errr.txt --cleanlog
+	#~/Unitex3.0beta/App/UnitexToolLogger RunLog $i  -s summ.txt -e errr.txt --cleanlog
+	if [ "$?" -ne "0" ] ;
 	then
 		echo "Error detected by valgrind on log $i"
 		exit 1
